0f0e435715c379508624302e3b8ee32769ffe2bf
Jan Achilles <jan.achilles@ostfalia.de>
2022-03-13 14:57:28 +0100

    edit

--- GUI_Beispiel/main.py
+++ GUI_Beispiel/main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from guiTest import gui_test
 from gui_anwendung import open_exe
 
